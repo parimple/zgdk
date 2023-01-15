d838525fa7e658ed5f372ed1a50d024671630d83
Patryk Pyzel <patrykpyzel@gmail.com>
2023-01-15 20:39:13 +0100

    test only commands

--- tests/commands/test_client.py
+++ tests/commands/test_client.py
@@ -2,21 +2,7 @@
 import pytest
 import discord.ext.test as dpytest 
 
-from ...cogs.commands import client
-
 class TestClient:
-
-    @staticmethod
-    def test_add_numbers():
-        assert client.add_numbers(1, 2, 3) == 6, "Should be 6"
-
-    @staticmethod
-    def test_add_numbers2():
-        assert client.add_numbers(1, 2, 5) == 8, "Should be 8"
-
-    @staticmethod
-    def test_add_numbers3():
-        assert client.add_numbers(1, 2, 10) == 13, "Should be 13"
 
     @staticmethod
     @pytest.mark.asyncio
