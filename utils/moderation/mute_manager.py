c5edbaf492eb85fe678b838f4edf52c009cad57e
patrykpyzel <patrykpyzel@gmail.com>
2025-06-05 20:42:37 +0000

    fix: muteimg nie zmienia już nicku na random - Usunięto automatyczne przywracanie nicku przy muteimg - muteimg i mutenick używają tej samej roli, więc logika była niepotrzebna - Nick zostanie zmieniony tylko przy mutenick, nie przy muteimg

--- utils/moderation/mute_manager.py
+++ utils/moderation/mute_manager.py
@@ -217,7 +217,8 @@ class MuteManager:
 
                 # Jeśli użytkownik ma włączone mutenick i już ma domyślny nick, upewnij się,
                 # że nick nie zostanie zmieniony przez dodanie/usunięcie ról
-                if has_nick_mute and is_default_nick and mute_type.type_name != MuteType.NICK:
+                # WAŻNE: Nie robimy tego dla IMG, bo używa tej samej roli co NICK
+                if has_nick_mute and is_default_nick and mute_type.type_name not in [MuteType.NICK, MuteType.IMG]:
                     # Sprawdź, czy nick został zmieniony przez dodanie roli i natychmiast przywróć
                     current_name = user.nick
                     if current_name != default_nick:
